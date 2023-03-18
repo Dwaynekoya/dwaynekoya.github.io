8b66659cd8b195aafdde818ae0d45171ccfe538b
Dwaynekoya <101224368+Dwaynekoya@users.noreply.github.com>
2023-03-18 21:57:28 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/* sideNav */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "20em";
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
