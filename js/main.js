90a47a157e36604a3079f1402f67b0f9444a4ee0
dwaynekoya <dwaynekoya@hotmail.com>
2023-06-04 00:08:22 +0200

    fixed responsiveness, changed info

--- js/main.js
+++ js/main.js
@@ -6,4 +6,16 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+/*popup images*/
+document.querySelectorAll('.imageMasonry img').forEach(image => {
+    image.onclick = () => {
+        //console.log(image.getAttribute('src'));
+        document.querySelector('.popup').style.display = 'block';
+        document.querySelector('.popup img').src = image.getAttribute('src');
+    }
+})
+document.querySelector('.popup span').onclick = () =>{
+    document.querySelector('.popup').style.display = 'none';
+}
+
 /* splash screens */ 
